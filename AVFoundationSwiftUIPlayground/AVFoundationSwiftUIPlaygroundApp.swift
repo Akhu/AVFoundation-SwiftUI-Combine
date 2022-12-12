3b84ee0705b854c45a6aa778c2bd67b045415fea
Anthony Da Cruz <me@anthony-dacruz.com>
2022-12-12 12:18:39 +0100

    Initial Commit

---
+++ AVFoundationSwiftUIPlayground/AVFoundationSwiftUIPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVFoundationSwiftUIPlaygroundApp.swift
+//  AVFoundationSwiftUIPlayground
+//
+//  Created by Anthony Da cruz on 12/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct AVFoundationSwiftUIPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
